86bcb51d641ac0afc5cdf32601fb03a325cec2ff
Yunus Balci <143970847+YunusBalci21@users.noreply.github.com>
2024-02-29 14:46:21 +0100

    Delete dm510_msgbox.h

--- dm510_msgbox.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DM510_MSGBOX_H__
-#define __DM510_MSGBOX_H__
-
-asmlinkage long sys_dm510_msgbox_put(const char __user *buffer, int length);
-asmlinkage long sys_dm510_msgbox_get(char __user *buffer, int length);
-
-#endif // __DM510_MSGBOX_H__
-
